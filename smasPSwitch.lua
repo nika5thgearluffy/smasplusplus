96adc0f730cd694047518053fe2f888d4942a376
The Sun God: Nika <219582684+nika5thgearluffy@users.noreply.github.com>
2026-04-20 23:40:19 -0600

    Use the first player instead of looping all players on "smasPSwitch.lua"

--- smasPSwitch.lua
+++ smasPSwitch.lua
@@ -13,14 +13,12 @@ local deathTimerPSwitch = 0
 
 function smasPSwitch.startPSwitchMusic() --Starts the P-Switch music.
     if not smasPSwitch.pSwitchMusicStarted then
-        for _,p in ipairs(Player.get()) do --Make sure all players are counted if i.e. using supermario128...
-            if not smasPSwitch.inNoPSwitchMusicPlayingSituations(p) then
-                SysManager.sendToConsole("P-Switch music activated!")
-                Sound.muteMusic(-1)
-                smasBooleans.musicMuted = true
-                pSwitchMusic = SFX.play(smasCharacterInfo.pSwitchMusic, Audio.MusicVolume() / 100, 0)
-                smasPSwitch.pSwitchMusicStarted = true
-            end
+        if not smasPSwitch.inNoPSwitchMusicPlayingSituations(player) then
+            SysManager.sendToConsole("P-Switch music activated!")
+            Sound.muteMusic(-1)
+            smasBooleans.musicMuted = true
+            pSwitchMusic = SFX.play(smasCharacterInfo.pSwitchMusic, Audio.MusicVolume() / 100, 0)
+            smasPSwitch.pSwitchMusicStarted = true
         end
     end
 end
@@ -38,13 +36,11 @@ function smasPSwitch.stopPSwitchMusic(resetLevelMusic) --Stops the P-Switch musi
     
     smasPSwitch.pSwitchMusicStarted = false
 
-    for _,p in ipairs(Player.get()) do --Make sure all players are counted if i.e. using supermario128...
-        if not smasPSwitch.inNoPSwitchMusicPlayingSituations(p) then
-            if resetLevelMusic then
-                SysManager.sendToConsole("P-Switch music deactivated!")
-                smasBooleans.musicMuted = false
-                Sound.restoreMusic(-1)
-            end
+    if not smasPSwitch.inNoPSwitchMusicPlayingSituations(player) then
+        if resetLevelMusic then
+            SysManager.sendToConsole("P-Switch music deactivated!")
+            smasBooleans.musicMuted = false
+            Sound.restoreMusic(-1)
         end
     end
 end
@@ -63,55 +59,46 @@ end
 
 function smasPSwitch.onDraw()
     Defines.pswitch_music = false
-    for _,p in ipairs(Player.get()) do --Make sure all players are counted if i.e. using supermario128...
-        
-        
-        
-        
-        --Start P-Switch/Stopwatch codes
-        if mem(0x00B2C62C, FIELD_WORD) == mem(0x00B2C87C, FIELD_WORD) - 1 then --P-Switch
-            smasPSwitch.startPSwitchMusic()
-            smasBooleans.pSwitchActive = true
+
+    --Start P-Switch/Stopwatch codes
+    if mem(0x00B2C62C, FIELD_WORD) == mem(0x00B2C87C, FIELD_WORD) - 1 then --P-Switch
+        smasPSwitch.startPSwitchMusic()
+        smasBooleans.pSwitchActive = true
+    end
+    if mem(0x00B2C62E, FIELD_WORD) == mem(0x00B2C87C, FIELD_WORD) - 1 then --Stopwatch
+        smasPSwitch.startPSwitchMusic()
+        smasBooleans.stopWatchActive = true
+    end
+    
+    
+    
+    --Make sure the music stops when collecting a starman, a megashroom, or winning a level if active
+    if smasPSwitch.inNoPSwitchMusicPlayingSituations(player) then
+        if pSwitchMusic ~= nil then
+            smasBooleans.musicMuted = false
+            pSwitchMusic:Stop()
         end
-        if mem(0x00B2C62E, FIELD_WORD) == mem(0x00B2C87C, FIELD_WORD) - 1 then --Stopwatch
-            smasPSwitch.startPSwitchMusic()
-            smasBooleans.stopWatchActive = true
+    end
+    
+    
+    
+    --Stop P-Switch/Stopwatch codes
+    if mem(0x00B2C62C, FIELD_WORD) == 1 then --P-Switch
+        smasBooleans.pSwitchActive = false
+        smasPSwitch.stopPSwitchMusic()
+    end
+    if mem(0x00B2C62E, FIELD_WORD) == 1 then --Stopwatch
+        smasBooleans.stopWatchActive = false
+        smasPSwitch.stopPSwitchMusic()
+    end
+    
+    
+    --Player death P-Switch stop codes
+    if not Playur.isAnyPlayerAlive() then
+        deathTimerPSwitch = deathTimerPSwitch + 1
+        if deathTimerPSwitch == 1 then
+            smasPSwitch.stopPSwitchMusic(false)
         end
-        
-        
-        
-        --Make sure the music stops when collecting a starman, a megashroom, or winning a level if active
-        if smasPSwitch.inNoPSwitchMusicPlayingSituations(p) then
-            if pSwitchMusic ~= nil then
-                smasBooleans.musicMuted = false
-                pSwitchMusic:Stop()
-            end
-        end
-        
-        
-        
-        --Stop P-Switch/Stopwatch codes
-        if mem(0x00B2C62C, FIELD_WORD) == 1 then --P-Switch
-            smasBooleans.pSwitchActive = false
-            smasPSwitch.stopPSwitchMusic()
-        end
-        if mem(0x00B2C62E, FIELD_WORD) == 1 then --Stopwatch
-            smasBooleans.stopWatchActive = false
-            smasPSwitch.stopPSwitchMusic()
-        end
-        
-        
-        --Player death P-Switch stop codes
-        if not Playur.isAnyPlayerAlive() then
-            deathTimerPSwitch = deathTimerPSwitch + 1
-            if deathTimerPSwitch == 1 then
-                smasPSwitch.stopPSwitchMusic(false)
-            end
-        end
-        
-        
-        
-        
     end
 end
 
