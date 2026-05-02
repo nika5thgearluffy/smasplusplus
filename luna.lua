705f3b8e25c2336b4ef5740eee0633c720c7de3b
The Sun God: Nika <219582684+nika5thgearluffy@users.noreply.github.com>
2026-05-02 12:25:35 -0500

    Misc.resolveFile only checks certain folders outside of the user files folder, so remove that check

--- luna.lua
+++ luna.lua
@@ -330,7 +330,7 @@ end
 
 function onDraw()
     -- Do this so that the PFP would need to be removed if non-existant
-    if SaveData.SMASPlusPlus.game.pfp ~= nil and Misc.resolveFile(SaveData.SMASPlusPlus.game.pfp) ~= nil and type(SaveData.SMASPlusPlus.game.pfp) == "string" then
+    if SaveData.SMASPlusPlus.game.pfp ~= nil and type(SaveData.SMASPlusPlus.game.pfp) == "string" then
         local pfpFileSize = File.getSize(SaveData.SMASPlusPlus.game.pfp)
         if pfpFileSize == -1 then
             SaveData.SMASPlusPlus.game.pfp = Misc.episodePath().."graphics/default_pfp.png"
