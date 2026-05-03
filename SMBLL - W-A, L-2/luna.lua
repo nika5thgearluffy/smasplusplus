fc1c0fd7479da70411afd3e1d241dede2ea749dd
The Sun God: Nika <219582684+nika5thgearluffy@users.noreply.github.com>
2026-05-03 04:03:36 -0500

    Going Underground SFXs changed to music versions
    Replace SFX versions of Going Underground themes with actual music themes, making the SFX versions obsolete
    
    Thus, IDs 139, 140, and 141 will be reserved for other SFXs in the future

--- SMBLL - W-A, L-2/luna.lua
+++ SMBLL - W-A, L-2/luna.lua
@@ -1,13 +1,5 @@
 local level_dependencies_normal= require("level_dependencies_normal")
 
-function onStart()
-    for _,p in ipairs(Player.get()) do
-        if SysManager.getEnteredCheckpointID() == 0 and p:mem(0x15E, FIELD_WORD) == 0 then
-            Sound.playSFX(139) --Going Underground
-        end
-    end
-end
-
 function onExit()
     for _,p in ipairs(Player.get()) do
         if p:mem(0x15E, FIELD_WORD) == 3 and p.forcedState == FORCEDSTATE_INVISIBLE then --SMBLL World B
