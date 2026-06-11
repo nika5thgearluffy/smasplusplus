cb130b7d9e9d61d3d16e0265969dee7ebd6a95dd
The Sun God: Nika <219582684+nika5thgearluffy@users.noreply.github.com>
2026-06-10 20:52:14 -0500

    Remove clearpipe mess that causes an error on SMB3 World 4 Fortress 2

--- SMB3 - W-4, L-Fortress2/luna.lua
+++ SMB3 - W-4, L-Fortress2/luna.lua
@@ -1,11 +1,4 @@
 local level_dependencies_normal= require("level_dependencies_normal")
-local clearpipe2 = require("clearpipe2")
-
-function onStart()
-    for i = 1, NPC_MAX_ID do
-        clearpipe2.unregisterNPC(i)
-    end
-end
 
 function onEvent(eventName)
     if eventName == "Boss Start" then
